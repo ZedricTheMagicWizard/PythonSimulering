4443b674940659779b4f3786fb8f095b49905fe9
Mads Loeber <zedricthemagicwizard@gmail.com>
2026-01-14 10:24:36 +0100

    Added main timer

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import physics
 import math_util as mutil
 import animationbox
 import matplotlib.pyplot as plt
-import time
 
 
 t=0
@@ -19,9 +18,9 @@ tMAX= 100
 
 #Planeter oprettes
 Jorden = physics.Planet('Jorden', 0.00597, 637.1, 1, 0, mutil.Vector(5,0))
-Mars = physics.Planet('Mars', 0.0006417, 338.95, 2, 0, mutil.Vector(0,0))
-Jupiter =physics.Planet('Jupiter', 1.898, 8991.1, 3, 0, mutil.Vector(0,0))
-Solen = physics.Planet('Solen', 1989, 69634,0, 0, mutil.Vector(0,0))
+Mars = physics.Planet('Mars', 0.0006417, 338.95, 2, 0, mutil.Vector(-2,0))
+Jupiter =physics.Planet('Jupiter', 1.898, 8991.1, 3, 0, mutil.Vector(2,0))
+Solen = physics.Planet('Solen', 1989, 69634,0, 0, mutil.Vector(-6,0))
 
 
 #Planeter tilføjes til liste
@@ -34,45 +33,18 @@ planetList= [
 box = animationbox.animationBox(-100, 100, -100, 100, "X", "Y", tMAX)   
 #points = box.ax.plot([1,2,3], [4,5,6], "ro")
 
-
-
-box.add_planet(Jorden)
-#box.add_planet(Mars)
+    
+for planet in planetList:
+    box.add_planet(planet)
 
 box.show(block=False)  
 
 
-for i in range(20):
-    Jorden.setXPosition(Jorden.getXPosition() + Jorden.getVelocityVector().x * dt)
-    box.update_all_planets()
-    plt.pause(dt)  # vent lidt, så det bevæger sig langsomt
-
-
-
-
-
-
-
-
-
-'''
-planetPoints = []
-for planet in planetList:
-    point = animationbox.planetPoint(box.ax, planet)
-    planetPoints.append(point)
-    
-while t<=tMAX:
-    planetSimulation = animationbox.simulatePlanets(tMAX, planetList, dt)
-
-    for planets in planetSimulation:
-
-        for pP in planetPoints:
-            pP.update()
-            
-    forces = physics.gravityOnObject(planetList)        
+while t < tMAX:
+    t = t + dt
+    plt.pause(dt)
     for planet in planetList:
-        force = forces[planet]
-        box.ax.arrow(planet.getXPosition(), planet.getYPosition(),
-         force.x*1e3, force.y*1e3, head_width=0.05, color='blue')
-        
-'''
+        planet.setXPosition(planet.getXPosition() + planet.getVelocityVector().x * dt)
+    box.update_all_planets()
+
+
